b29bdaffd7bad86bc8ce5c424bbbb204aa1fb6cf
unknown <pateldhiraj782@gmail.com>
2021-02-12 22:54:24 +0530

    day-12 ListView Builder and card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/itemWidget.dart';
 
 //Day 11 learned context and constrain
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final itemList = List.generate(20, (index) => CatalogModel.products[0]);
+
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
@@ -14,8 +18,13 @@ class HomePage extends StatelessWidget {
             "Catalog App",
           ),
         ),
-        body: Container(
-          child: Center(child: Text("welcome to Catalog App")),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+              itemCount: itemList.length,
+              itemBuilder: (context, index) {
+                return ItemWidget(item: itemList[index]);
+              }),
         ),
         drawer: MyDrawer());
   }
